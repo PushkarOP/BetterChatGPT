1971f253ec2b8e3e4322d44d583eb720e8fd596e
Pushkar Singh <84075300+PushkarOP@users.noreply.github.com>
2024-10-02 16:57:40 +0530

    Update MenuOptions.tsx

--- src/components/Menu/MenuOptions/MenuOptions.tsx
+++ src/components/Menu/MenuOptions/MenuOptions.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import useStore from '@store/store';
 
-import Api from './Api';
-import Me from './Me';
 import AboutMenu from '@components/AboutMenu';
 import ImportExportChat from '@components/ImportExportChat';
 import SettingsMenu from '@components/SettingsMenu';
