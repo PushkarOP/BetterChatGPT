daf4e6c61570f6a8b6b1d8b748c79743fe39be90
Pushkar Singh <84075300+PushkarOP@users.noreply.github.com>
2024-10-02 18:28:44 +0530

    Update ApiMenu.tsx

--- src/components/ApiMenu/ApiMenu.tsx
+++ src/components/ApiMenu/ApiMenu.tsx
@@ -40,75 +40,7 @@ const ApiMenu = ({
     _setCustomEndpoint((prev) => !prev);
   };
 
-  return (
-    <PopupModal
-      title={t('api') as string}
-      setIsModalOpen={setIsModalOpen}
-      handleConfirm={handleSave}
-    >
-      <div className='p-6 border-b border-gray-200 dark:border-gray-600'>
-       
-        <div className='flex gap-2 items-center mb-6'>
-          <div className='min-w-fit text-gray-900 dark:text-gray-300 text-sm'>
-            {t('apiEndpoint.inputLabel', { ns: 'api' })}
-          </div>
-          {_customEndpoint ? (
-            <input
-              type='text'
-              className='text-gray-800 dark:text-white p-3 text-sm border-none bg-gray-200 dark:bg-gray-600 rounded-md m-0 w-full mr-0 h-8 focus:outline-none'
-              value={_apiEndpoint}
-              onChange={(e) => {
-                _setApiEndpoint(e.target.value);
-              }}
-            />
-          ) : (
-            <ApiEndpointSelector
-              _apiEndpoint={_apiEndpoint}
-              _setApiEndpoint={_setApiEndpoint}
-            />
-          )}
-        </div>
-
-        <div className='flex gap-2 items-center justify-center mt-2'>
-          <div className='min-w-fit text-gray-900 dark:text-gray-300 text-sm'>
-            {t('apiKey.inputLabel', { ns: 'api' })}
-          </div>
-          <input
-            type='text'
-            className='text-gray-800 dark:text-white p-3 text-sm border-none bg-gray-200 dark:bg-gray-600 rounded-md m-0 w-full mr-0 h-8 focus:outline-none'
-            value={_apiKey}
-            onChange={(e) => {
-              _setApiKey(e.target.value);
-            }}
-          />
-        </div>
-
-        <div className='min-w-fit text-gray-900 dark:text-gray-300 text-sm flex flex-col gap-3 leading-relaxed'>
-          <p className='mt-4'>
-            <Trans
-              i18nKey='apiKey.howTo'
-              ns='api'
-              components={[
-                <a
-                  href='https://platform.openai.com/account/api-keys'
-                  className='link'
-                  target='_blank'
-                />,
-              ]}
-            />
-          </p>
-
-          <p>{t('securityMessage', { ns: 'api' })}</p>
-
-          <p>{t('apiEndpoint.description', { ns: 'api' })}</p>
-
-          <p>{t('apiEndpoint.warn', { ns: 'api' })}</p>
-        </div>
-      </div>
-    </PopupModal>
-  );
-};
-
+  return 
 const ApiEndpointSelector = ({
   _apiEndpoint,
   _setApiEndpoint,
